ae18766fcf99dec9469a8f7ab6d0f9a83fe7298f
Aspyn Palatnick <paluter@yahoo.com>
2020-08-07 12:50:05 -0400

    update bacteria runtime plot

--- validation_for_paper/full_validation/bacteria_simulations/generateConcisePlotForMultipleGenomes.py
+++ validation_for_paper/full_validation/bacteria_simulations/generateConcisePlotForMultipleGenomes.py
@@ -4,68 +4,93 @@ import matplotlib.pyplot as plt
 import json
 
 LINE_WIDTH = 3
-GEN_CONCISE_REPORT_PATH = '/Users/Stuckinaboot/Downloads/iGenomics/validation_for_paper/full_validation/bacteria_simulations/generateConciseReportFilesForMultipleGenomes.py'
+GEN_CONCISE_REPORT_PATH = "/Users/stuckinaboot/Documents/projects/iGenomics.nosync/validation_for_paper/full_validation/bacteria_simulations/generateConciseReportFilesForMultipleGenomes.py"
 
 currPath = os.path.abspath(argv[1])
-dwgPath = os.path.abspath(argv[2]) #Should be the path to the directory containing all the gen output folders
-igPath = os.path.abspath(argv[3]) #Should be the path to the directory containing the tree of iGenomics output folders
+dwgPath = os.path.abspath(
+    argv[2]
+)  # Should be the path to the directory containing all the gen output folders
+igPath = os.path.abspath(
+    argv[3]
+)  # Should be the path to the directory containing the tree of iGenomics output folders
 
-conciseReportFilePath = os.path.join(currPath, 'concise_report.cpr')
-os.system('python ' + GEN_CONCISE_REPORT_PATH + ' ' + currPath + ' ' + dwgPath + ' ' + igPath + ' > ' + conciseReportFilePath)
+conciseReportFilePath = os.path.join(currPath, "concise_report.cpr")
+os.system(
+    "python3 "
+    + GEN_CONCISE_REPORT_PATH
+    + " "
+    + currPath
+    + " "
+    + dwgPath
+    + " "
+    + igPath
+    + " > "
+    + conciseReportFilePath
+)
 
 dataPoints = []
 with open(conciseReportFilePath) as conciseFile:
-	for line in conciseFile.readlines():
-		line = line.strip('\n').strip('\r')
-		info = json.loads(line)
-		dataPoints.append(info)
+    for line in conciseFile.readlines():
+        line = line.strip("\n").strip("\r")
+        info = json.loads(line)
+        dataPoints.append(info)
 
 plotDataPoints = {}
 for data in dataPoints:
-	for runtimeKey in data['runtimes']:
-		if runtimeKey not in plotDataPoints:
-			plotDataPoints[runtimeKey] = {}
-		readLen = data['read len']
-		if readLen not in plotDataPoints[runtimeKey]:
-			plotDataPoints[runtimeKey][readLen] = []
-		plotDataPoints[runtimeKey][readLen].append(
-			{'runtime': data['runtimes'][runtimeKey],
-			'reference length': data['reference length']
-			})
+    for runtimeKey in data["runtimes"]:
+        if runtimeKey not in plotDataPoints:
+            plotDataPoints[runtimeKey] = {}
+        readLen = data["read len"]
+        if readLen not in plotDataPoints[runtimeKey]:
+            plotDataPoints[runtimeKey][readLen] = []
+        plotDataPoints[runtimeKey][readLen].append(
+            {
+                "runtime": data["runtimes"][runtimeKey],
+                "reference length": data["reference length"],
+            }
+        )
 
 # print plotDataPoints
-plotPath = os.path.join(currPath, 'concise_plot.pdf')
+plotPath = os.path.join(currPath, "concise_plot.pdf")
 
-cm = plt.get_cmap('rainbow')
+cm = plt.get_cmap("rainbow")
 NUM_COLORS = 4
 
-plt.axes().set_color_cycle([cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)])
-plt.title('Runtime vs. Reference Length')
-plt.xlabel('Reference Length (bp)')
-plt.ylabel('Runtime (s)')
+plt.axes().set_prop_cycle(color=[cm(1.0 * i / NUM_COLORS) for i in range(NUM_COLORS)])
+plt.title("Runtime vs. Reference Length")
+plt.xlabel("Reference Length (bp)")
+plt.ylabel("Runtime (s)")
 
 # #a) Plot iGenomics runtime
-colorsForVerticalLines = ['red', 'orange', 'green', 'blue', 'indigo']
-labelsForVerticalLines = ['PhiX174 (5386bp)', 'Zika (10807bp)', 'H1N1 (13382bp)', 'H3N2 (13568bp)', 'Ebola (18957bp)']
+colorsForVerticalLines = ["red", "orange", "green", "blue", "indigo"]
+labelsForVerticalLines = [
+    "PhiX174 (5386bp)",
+    "Zika (10807bp)",
+    "H1N1 (13382bp)",
+    "H3N2 (13568bp)",
+    "Ebola (18957bp)",
+]
 refLinesAlreadyShowing = False
 for runtimeKey in plotDataPoints:
-	for readLen in plotDataPoints[runtimeKey]:
-		plotDataPt = plotDataPoints[runtimeKey][readLen]
-		sortedPlotDataPt = sorted(plotDataPt, key=lambda x: x['reference length'])
-		refLens = [singlePt['reference length'] for singlePt in sortedPlotDataPt]
-		if refLinesAlreadyShowing == False:
-			for i, refLen in enumerate(refLens):
-				plt.axvline(refLen, color=colorsForVerticalLines[i], linestyle='dashed', label=labelsForVerticalLines[i])
-			refLinesAlreadyShowing = True
-		runtimes = [singlePt['runtime'] for singlePt in sortedPlotDataPt] 
-		print sortedPlotDataPt
-		label = runtimeKey + ' | ' + str(readLen) + 'bp'
-		plt.plot(refLens, runtimes, label=label, linewidth=LINE_WIDTH)
+    for readLen in plotDataPoints[runtimeKey]:
+        plotDataPt = plotDataPoints[runtimeKey][readLen]
+        sortedPlotDataPt = sorted(plotDataPt, key=lambda x: x["reference length"])
+        refLens = [singlePt["reference length"] for singlePt in sortedPlotDataPt]
+        if refLinesAlreadyShowing == False:
+            for i, refLen in enumerate(refLens):
+                plt.axvline(
+                    refLen,
+                    color=colorsForVerticalLines[i],
+                    linestyle="dashed",
+                    label=labelsForVerticalLines[i],
+                )
+            refLinesAlreadyShowing = True
+        runtimes = [singlePt["runtime"] for singlePt in sortedPlotDataPt]
+        print(runtimeKey, sortedPlotDataPt)
+        label = runtimeKey + " | " + str(readLen) + "bp"
+        plt.plot(refLens, runtimes, label=label, linewidth=LINE_WIDTH)
 
-plt.legend(loc=0,prop={'size':6})
+plt.legend(loc=0, prop={"size": 6})
 plt.savefig(plotPath)
 plt.gcf().clear()
-
-
-
 
--- validation_for_paper/full_validation/bacteria_simulations/generateConciseReportFilesForMultipleGenomes.py
+++ validation_for_paper/full_validation/bacteria_simulations/generateConciseReportFilesForMultipleGenomes.py
@@ -2,7 +2,7 @@ from sys import argv
 import os
 import json
 
-'''
+"""
 Concise Report File Format (.crf):
 Contains a JSON object on each line that looks like the following:
 	{
@@ -14,117 +14,129 @@ Contains a JSON object on each line that looks like the following:
 		'runtimes': {'iG runtime': iGenomics runtime, 'bwa': BWA runtime}
 	}
 	where each JSON object represents one genome
-'''
+"""
 
-COMPARE_MUTS_PATH = '/Users/Stuckinaboot/Downloads/iGenomics/validation_for_paper/full_validation/full_validation_utilities/my_scripts/compare_mutations_vcfs.py'
+COMPARE_MUTS_PATH = "/Users/stuckinaboot/Documents/projects/iGenomics.nosync/validation_for_paper/full_validation/full_validation_utilities/my_scripts/compare_mutations_vcfs.py"
+
 
 def createDirectoryAtPath(path):
-	try:
-		os.makedirs(path)
-	except:
-		pass
+    try:
+        os.makedirs(path)
+    except:
+        pass
+
 
 currPath = os.path.abspath(argv[1])
-dwgPath = os.path.abspath(argv[2]) #Should be the path to the directory containing all the gen output folders
-igPath = os.path.abspath(argv[3]) #Should be the path to the directory containing the tree of iGenomics output folders
+dwgPath = os.path.abspath(
+    argv[2]
+)  # Should be the path to the directory containing all the gen output folders
+igPath = os.path.abspath(
+    argv[3]
+)  # Should be the path to the directory containing the tree of iGenomics output folders
 # currPath = os.path.join(currPath, 'concise_reports')
 
 createDirectoryAtPath(currPath)
 
+
 def acpFileToDict(filePath):
-	info = {}
-	with open(filePath) as file:
-		keys = []
-		for i, line in enumerate(file.readlines()):
-			if i == 0:
-				keys = line.split('\t')
-			elif i == 1:
-				values = line.split('\t')
-				for key, value in zip(keys, values):
-					info[key] = value
-				return info
+    info = {}
+    with open(filePath) as file:
+        keys = []
+        for i, line in enumerate(file.readlines()):
+            if i == 0:
+                keys = line.split("\t")
+            elif i == 1:
+                values = line.split("\t")
+                for key, value in zip(keys, values):
+                    info[key] = value
+                return info
+
 
 def runtimeFromREADMEdig(readmeDigPath):
-	with open(readmeDigPath) as file:
-		runtimeLine = file.readline()
-		runtimeStr = runtimeLine.split('\t')[1].strip('s\n')
-		runtimeComponents = runtimeStr.split('m')
-		actualRuntime = int(runtimeComponents[0]) * 60.0 + float(runtimeComponents[1])
-		return actualRuntime
+    with open(readmeDigPath) as file:
+        runtimeLine = file.readline()
+        runtimeStr = runtimeLine.split("\t")[1].strip("s\n")
+        runtimeComponents = runtimeStr.split("m")
+        actualRuntime = int(runtimeComponents[0]) * 60.0 + float(runtimeComponents[1])
+        return actualRuntime
+
 
 def accuracyFromMutsOutFile(mutFilePath):
-	with open(mutFilePath) as file:
-		info = {}
-		for line in file.readlines():
-			if ('PASSED:' in line and '>>' not in line) or 'LEN' in line:
-				components = line.split(': ')
-				info[components[0]] = int(components[1])
-		recall = float(info['PASSED']) / info['LEN BWA']
-		precision = float(info['PASSED']) / info['LEN iG']
-		accuracy = (2 * precision * recall) / float(precision + recall)
-		return accuracy
+    with open(mutFilePath) as file:
+        info = {}
+        for line in file.readlines():
+            if ("PASSED:" in line and ">>" not in line) or "LEN" in line:
+                components = line.split(": ")
+                info[components[0]] = int(components[1])
+        recall = float(info["PASSED"]) / info["LEN BWA"]
+        precision = float(info["PASSED"]) / info["LEN iG"]
+        accuracy = (2 * precision * recall) / float(precision + recall)
+        return accuracy
+
 
 def lengthFromReferenceFile(refFilePath):
-	with open(refFilePath) as file:
-		length = 0
-		for line in file.readlines():
-			line = line.strip('\n').strip('\r')
-			if len(line) > 0 and line[0] != '>':
-				length += len(line)
-		return length
+    with open(refFilePath) as file:
+        length = 0
+        for line in file.readlines():
+            line = line.strip("\n").strip("\r")
+            if len(line) > 0 and line[0] != ">":
+                length += len(line)
+        return length
+
 
 for igFolder in os.listdir(igPath):
-	currIGPath = os.path.join(igPath, igFolder)
-	dwgFolder = 'generation_output_' + igFolder.replace('.fa', '')
-	currDWGPath = os.path.join(dwgPath, dwgFolder)
-        for readLenPath in os.listdir(currIGPath):
+    currIGPath = os.path.join(igPath, igFolder)
+    dwgFolder = "generation_output_" + igFolder.replace(".fa", "")
+    currDWGPath = os.path.join(dwgPath, dwgFolder)
+    for readLenPath in os.listdir(currIGPath):
 
-		referenceFilePath = os.path.join(currPath, igFolder + '.fa')
-                referenceLen = lengthFromReferenceFile(referenceFilePath)
-		infoDict = {
-					'read len': readLenPath.strip('read_len').strip('bp'),
-					'reference': igFolder,
-					'reference length': referenceLen
-					}
-		
-		currIGPath = os.path.join(os.path.join(igPath, igFolder), readLenPath)
-		igFolderFiles = []
-		for (dirpath, dirnames, filenames) in os.walk(currIGPath):
-			igFolderFiles.extend([os.path.join(dirpath, name) for name in filenames])
-		
-		currDWGPath = os.path.join(os.path.join(dwgPath, dwgFolder), readLenPath)
-		dwgFolderFiles = []
-		for (dirpath, dirnames, filenames) in os.walk(currDWGPath):
-			dwgFolderFiles.extend([os.path.join(dirpath, name) for name in filenames])
+        referenceFilePath = os.path.join(currPath, igFolder + ".fa")
+        referenceLen = lengthFromReferenceFile(referenceFilePath)
+        infoDict = {
+            "read len": readLenPath.strip("read_len").strip("bp"),
+            "reference": igFolder,
+            "reference length": referenceLen,
+        }
 
+        currIGPath = os.path.join(os.path.join(igPath, igFolder), readLenPath)
+        igFolderFiles = []
+        for (dirpath, dirnames, filenames) in os.walk(currIGPath):
+            igFolderFiles.extend([os.path.join(dirpath, name) for name in filenames])
 
-		infoDict['runtimes'] = {}
-		igMutationsFilePath = ''
-		for filePath in igFolderFiles:
-			if '.acp' in filePath:
-				acpDict = acpFileToDict(filePath)
-				infoDict['runtimes']['iG'] = acpDict['RT']
-				infoDict['number of reads'] = acpDict['RC']
-			elif '.normalized.ig.vcf' in filePath:
-				igMutationsFilePath = filePath
+        currDWGPath = os.path.join(os.path.join(dwgPath, dwgFolder), readLenPath)
+        dwgFolderFiles = []
+        for (dirpath, dirnames, filenames) in os.walk(currDWGPath):
+            dwgFolderFiles.extend([os.path.join(dirpath, name) for name in filenames])
 
-		dwgMutationsFilePath = ''
-		for filePath in dwgFolderFiles:
-			if '.normalized.dwg.vcf' in filePath:
-				dwgMutationsFilePath = filePath
-			elif 'README.dig' in filePath:
-				infoDict['runtimes']['bwa'] = runtimeFromREADMEdig(filePath)
+        infoDict["runtimes"] = {}
+        igMutationsFilePath = ""
+        for filePath in igFolderFiles:
+            if ".acp" in filePath:
+                acpDict = acpFileToDict(filePath)
+                infoDict["runtimes"]["iG"] = acpDict["RT"]
+                infoDict["number of reads"] = acpDict["RC"]
+            elif ".normalized.ig.vcf" in filePath:
+                igMutationsFilePath = filePath
 
-		mutsOutPath = os.path.join(currPath, 'muts.out')
-                os.system('python ' + COMPARE_MUTS_PATH + ' ' + dwgMutationsFilePath + ' ' + igMutationsFilePath + ' > ' + mutsOutPath)
-		infoDict['mutation accuracy'] = accuracyFromMutsOutFile(mutsOutPath)
-		os.system('rm ' + mutsOutPath)
-		print json.dumps(infoDict)
+        dwgMutationsFilePath = ""
+        for filePath in dwgFolderFiles:
+            if ".normalized.dwg.vcf" in filePath:
+                dwgMutationsFilePath = filePath
+            elif "README.dig" in filePath:
+                infoDict["runtimes"]["bwa"] = runtimeFromREADMEdig(filePath)
 
-
-
-
-
-
-
+        mutsOutPath = os.path.join(currPath, "muts.out")
+        os.system(
+            "python3 "
+            + COMPARE_MUTS_PATH
+            + " "
+            + dwgMutationsFilePath
+            + " "
+            + igMutationsFilePath
+            + " > "
+            + mutsOutPath
+        )
+        infoDict["mutation accuracy"] = accuracyFromMutsOutFile(mutsOutPath)
+        os.system("rm " + mutsOutPath)
+        print(json.dumps(infoDict))
 
