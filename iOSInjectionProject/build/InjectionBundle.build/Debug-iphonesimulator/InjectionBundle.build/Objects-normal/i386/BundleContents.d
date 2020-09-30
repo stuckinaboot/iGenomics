5cba331d477cd0d1953cf6575afaae8a009c2ded
Aspyn Palatnick <paluter@yahoo.com>
2020-09-30 13:42:23 -0400

    cleanup

--- iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/i386/BundleContents.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/Stuckinaboot/Documents/iGenomics/iOSInjectionProject/BundleContents.m \
-  /Applications/Injection\ Plugin.app/Contents/Resources/BundleInjection.h \
-  /Applications/Injection\ Plugin.app/Contents/Resources/BundleInterface.h \
-  /Users/Stuckinaboot/Documents/iGenomics/iOSInjectionProject/BundleContents.h
